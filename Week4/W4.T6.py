5b580c78eeef78f6290c9c931cd8ecc6afa6fc2b
sarmad al-obaidi <sarmad.shatti@gmail.com>
2025-11-19 18:43:45 +0200

    Enhance program with input validation and loops
    Added input for starting, stopping, and inspection points. Implemented two loops for inspection with break and continue statements.

--- Week4/W4.T6.py
+++ Week4/W4.T6.py
@@ -1,15 +1,34 @@
-print("program starting") 
+print("Program starting.")
 
-value = int(input("Inserta positive integer: "))
-rounds = 0
+start = int(input("Insert starting point: "))
+stop = int(input("Insert stopping point: "))
+inspect = int(input("Insert inspection point: "))
 
-while value != 1:
-    print(value, end=" ")
-    if value % 2 == 0:
-        value = value // 2
-    else: 
-        value = value * 3 + 1
-    rounds += 1
+if start >= stop:
+    print("Starting point value must be less than the stopping point value.")
+elif not (start <= inspect <= stop):
+    print("Inspection value must be within the range of start and stop.")
+else:
+    print("First loop- inspection with break:")
+    first = True
+    for i in range(start, stop):
+        if i == inspect:
+            break
+        if not first:
+            print("・", end="")
+        print(i, end="")
+        first = False
+    print()
 
-print("1")
-print(f"Sequnece had {rounds} total steps.")        
+    print("Second loop - inspection with continue:")
+    first = True
+    for i in range(start, stop):
+        if i == inspect:
+            continue
+        if not first:
+            print("・", end="")
+        print(i, end="")
+        first = False
+    print()
+
+print("Program ending.")
